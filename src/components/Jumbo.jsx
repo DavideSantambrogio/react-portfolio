aeb61455e22e9f8387d1afb4efef5c62c933923a
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-04-12 15:31:22 +0200

    nav ad jumbo

---
+++ src/components/Jumbo.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+
+
+function Jumbo() {
+
+
+  return (
+    <>
+      <div className="my-bg-primary">
+        <div className="container my-container p-5">
+          <h2 className="p-2">Ciao sono</h2>
+          <h1 className="p-2">Davide Santambrogio</h1>
+          <h4 className="p-2">Full stack Web developer</h4>
+          <button type="button" className="btn btn-secondary mt-5">Contattami</button>
+        </div>
+      </div>
+
+
+      <div className="container mt-4">
+
+      </div>
+    </>
+  );
+}
+
+export default Jumbo;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+
+function Navbar() {
+
+
+    return (
+        <>
+
+            <nav className="navbar navbar-expand-lg fixed-top bg-primary  " data-bs-theme="dark">
+                <div className="container ">
+                    <h3  className="text-light" href="#">Portfolio</h3>
+                    
+                    <div className="" id="navbarNavAltMarkup">
+                        <div className="navbar-nav gap-5">
+                            <a className="nav-link text-light" aria-current="page" href="#">Chi sono</a>
+                            <a className="nav-link text-light" href="#">Features</a>
+                            <a className="nav-link text-light" href="#">Pricing</a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+        </>
+    )
+}
+
+export default Navbar
