13d06ab1fc942d9b867439dda1c7b25848ee1ef3
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-04-29 11:02:22 +0200

    add presentation responsive

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function Presentation() {
-
-
     return (
         <>
             <div id='about' className="my-bg-primary">
@@ -10,24 +8,21 @@ function Presentation() {
                     <h4 className='my-text-color pb-4'>UN PO' DI ME</h4>
                     
                     <div className="row">
-                        <div className="col-6">
-                        <h1 className='pb-4'>Chi sono?</h1>
-                            <p >
+                        <div className="col-12 col-md-6">
+                            <h1 className='pb-4'>Chi sono?</h1>
+                            <p>
                                 Ciao, sono Davide Santambrogio, un Full Stack Web Developer. <br />
                                 Dopo aver completato gli studi in agraria e aver lavorato presso un vivaio, ho realizzato che il mio vero interesse risiedeva altrove. <br />
                                 Per caso, ho scoperto il mondo della programmazione e ho subito compreso che era la direzione giusta per me. Questo campo non solo ha dato spazio alle mie idee, ma mi ha anche permesso di esprimere la mia creatività in modi che non avrei mai immaginato.Sono sempre alla ricerca di nuove sfide e opportunità per ampliare le mie conoscenze e competenze. La mia curiosità insaziabile e la passione per la programmazione mi spingono costantemente a cercare soluzioni creative e innovative. Sono determinato a trasformare idee in progetti concreti e funzionali, sfruttando al massimo le mie abilità e la mia inventiva.
                             </p>
                         </div>
 
-                        <img className="col-6" src="src\assets\img\pc.png" alt="" />
+                        <div className="col-12 col-md-6 mt-auto">
+                            {/* Imposta l'immagine con la classe img-fluid per renderla responsive */}
+                            <img className="img-fluid d-md-block d-none  " src="src\assets\img\pc.png" alt="" />
+                        </div>
                     </div>
-
-
-
-
                 </div>
-
-
             </div>
         </>
     );
