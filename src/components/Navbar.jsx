f82d11d1a114c866e1e3a5b341b8fcf2c381d646
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-04-29 10:18:34 +0200

    add: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,25 @@
-import { Link } from "react-router-dom"
-
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <>
-
-            <nav className="navbar navbar-expand-lg sticky-top pt-4 pb-3  " data-bs-theme="dark">
-                <div className="container ">
-                    <h3 className="text-light" href="#">Portfolio</h3>
-                    <div className="" id="navbarNavAltMarkup">
-                        <div className="navbar-nav gap-5">
-                            <Link className="nav-link text-light" to={'/'}>Home</Link>
-                            <a className="nav-link text-light" aria-current="page" href="#about">Chi sono</a>
-                            <a className="nav-link text-light" aria-current="page" href="#backgroud">Esperienze e Formazione</a>
-                            <Link className="nav-link text-light" to={'/projects'}>Progetti</Link>
-
-                        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark sticky-top pt-4 pb-3 ">
+            <div className="container">
+                <Link className="navbar-brand" to={'/'}>Portfolio</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <Link className="nav-link" to={'/'}>Home</Link>
+                        <a className="nav-link" href="#about">Chi sono</a>
+                        <a className="nav-link" href="#backgroud">Esperienze e Formazione</a>
+                        <Link className="nav-link" to={'/projects'}>Progetti</Link>
                     </div>
                 </div>
-            </nav>
-
-
-        </>
-    )
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
