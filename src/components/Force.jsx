084ac7d02bcbbf9e681540ee7f392737b643bf21
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-04-29 10:36:08 +0200

    add: responsive jumbo e force

--- src/components/Force.jsx
+++ src/components/Force.jsx
@@ -1,48 +1,46 @@
+import React from 'react';
 
 function Force() {
-
-
     return (
         <>
             <div className="my-bg-secondary">
-                <div className="container pb-5 pt-5 ">
+                <div className="container pb-5 pt-5">
                     <p className="fs-4 mb-5 my-text-color">
                         I MIEI PUNTI DI FORZA
                     </p>
-                    <div className="row justify-content-between g-5">
-                        <div className="col-4 text-white">
-                            <p className="fs-4 ">
+                    <div className="row">
+                        {/* Colonna per Curiosità */}
+                        <div className="col-lg-4 text-white mb-5">
+                            <p className="fs-4">
                                 CURIOSITÀ
                             </p>
                             <p>
-                            Sono costantemente spinto dalla curiosità nel mondo dello sviluppo web, sempre desideroso di esplorare nuovi linguaggi, framework e tecniche. Ogni progetto rappresenta un'opportunità per approfondire le mie conoscenze e soddisfare la mia sete di apprendimento nel vasto panorama dello sviluppo web.
+                                Sono costantemente spinto dalla curiosità nel mondo dello sviluppo web, sempre desideroso di esplorare nuovi linguaggi, framework e tecniche. Ogni progetto rappresenta un'opportunità per approfondire le mie conoscenze e soddisfare la mia sete di apprendimento nel vasto panorama dello sviluppo web.
                             </p>
                         </div>
-                        <div className="col-4 text-white">
-                            <p className="fs-4 ">
+                        {/* Colonna per Creatività */}
+                        <div className="col-lg-4 text-white mb-5">
+                            <p className="fs-4">
                                 CREATIVITÀ
                             </p>
                             <p>
-                            La mia passione per la programmazione web si esprime attraverso la creatività nel trovare soluzioni originali e intuitive per i problemi che incontro. Mi piace pensare fuori dagli schemi e trasformare idee astratte in esperienze utente coinvolgenti e funzionalità innovative. La creatività è la mia guida nel dare vita a progetti web che siano non solo funzionali, ma anche esteticamente gradevoli e intuitivi.
+                                La mia passione per la programmazione web si esprime attraverso la creatività nel trovare soluzioni originali e intuitive per i problemi che incontro. Mi piace pensare fuori dagli schemi e trasformare idee astratte in esperienze utente coinvolgenti e funzionalità innovative. La creatività è la mia guida nel dare vita a progetti web che siano non solo funzionali, ma anche esteticamente gradevoli e intuitivi.
                             </p>
                         </div>
-                        <div className="col-4 text-white">
-                            <p className="fs-4 ">
+                        {/* Colonna per Collaborazione */}
+                        <div className="col-lg-4 text-white mb-5">
+                            <p className="fs-4">
                                 COLLABORAZIONE
                             </p>
                             <p>
-                            Nel contesto dello sviluppo web, essere collaborativi significa avere la capacità di lavorare efficacemente in squadra, ascoltando attivamente le idee degli altri membri e contribuendo con le proprie competenze per il successo del progetto.  Sono aperto al feedback e alla condivisione delle conoscenze, e mi impegno a mantenere una comunicazione chiara e trasparente. 
+                                Nel contesto dello sviluppo web, essere collaborativi significa avere la capacità di lavorare efficacemente in squadra, ascoltando attivamente le idee degli altri membri e contribuendo con le proprie competenze per il successo del progetto. Sono aperto al feedback e alla condivisione delle conoscenze, e mi impegno a mantenere una comunicazione chiara e trasparente.
                             </p>
                         </div>
                     </div>
-
                 </div>
             </div>
-
-
-
         </>
     )
 }
 
-export default Force
+export default Force;
--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -2,41 +2,47 @@ import React from 'react';
 import { useGlobalContext } from '../GlobalContext';
 
 function Jumbo() {
-    const { technologies, githubRepos } = useGlobalContext(); // Ottieni le tecnologie dallo store globale
+    // Ottieni le tecnologie e i repository GitHub dallo store globale
+    const { technologies, githubRepos } = useGlobalContext(); 
 
     return (
         <>
+            {/* Jumbotron */}
             <div className="my-bg-primary">
-                <div className="container  px-5 pt-5">
+                <div className="container px-5 pt-5">
                     <div className="row">
-                        <div className="col-6 pb-5">
+                        
+                        {/* Colonna sinistra */}
+                        <div className="col-lg-6 pb-5">
                             <h2>Ciao sono</h2>
-                            <p className=" fs-1">Davide Santambrogio</p>
+                            <p className="fs-1">Davide Santambrogio</p>
                             <h4 className="p-2">Full stack Web developer</h4>
+                            
+                            {/* Pulsante di contatto */}                            
                             <a type="button" href='#contactUs' className="btn btn-secondary mt-3 px-5 py-2">Contattami</a>
-                            <div className="row pt-5 justify-content-around">
-                                <div className="col-3 text-center">
+                            
+                            {/* Contatori per tecnologie e repository */}
+                            <div className="row pt-5 justify-content-around">                                
+                                {/* Contatore per le tecnologie */}
+                                <div className="col-6 col-lg-3 text-center">
                                     <strong className='my-text-color fs-1'>{technologies.length}</strong>
-                                    <p className='my-text-color fs-4'>Tecnologie utlilizzate</p>
+                                    <p className='my-text-color fs-4'>Tecnologie utilizzate</p>                                
                                 </div>
-                                <div className="col-3 text-center">
 
+                                {/* Contatore per i repository GitHub */}
+                                <div className="col-6 col-lg-3 text-center">
                                     <strong className='my-text-color fs-1'>{githubRepos.length}</strong>
-                                    <p className='my-text-color fs-4'> Progetti pubblici</p>
+                                    <p className='my-text-color fs-4'>Progetti pubblici</p>
                                 </div>
                             </div>
-
                         </div>
-                        <div className='col-6' >
-                            {/* <img src="src\assets\img\file__1_-removebg-preview.png" alt="" />   */}
-
+                        
+                        {/* Colonna destra - Immagine (mostrata solo su schermi grandi) */}
+                        <div className='col-lg-6 d-none d-lg-block'>
+                            <img src="src\assets\img\file__1_-removebg-preview.png" alt="" />
                         </div>
                     </div>
-
-
                 </div>
-
-
             </div>
         </>
     );
