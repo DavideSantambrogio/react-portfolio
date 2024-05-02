4fa14cbd41a514a615238ff2cddb0271960673d2
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-05-02 11:21:43 +0200

    add:public imaged

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import VueImages from 'vite-plugin-vue-images';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VueImages()
+    VueImages(),
+    legacy({
+      targets: ['defaults', 'not IE 11'] // Configura i target per cui desideri supporto legacy
+    })
   ]
 });
